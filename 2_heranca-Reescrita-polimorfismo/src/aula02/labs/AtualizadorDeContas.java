d55847aa6c807d3f6f16dfbe6fb654c0972b8217
José Malcher Jr <malcher.malch@gmail.com>
2016-09-02 14:54:03 -0300

    Aula 02 - Herança e reescrita e polimorfismo

---
+++ 2_heranca-Reescrita-polimorfismo/src/aula02/labs/AtualizadorDeContas.java
@@ -0,0 +1,48 @@
+package aula02.labs;
+
+public class AtualizadorDeContas {
+	private double saldoTotal = 0;
+	private double selic;
+	
+	public AtualizadorDeContas(double selic) {
+		this.selic = selic;
+	}
+	
+	
+	public void roda(Conta c){
+		System.out.println("Saldo Antes = R$" + c.getSaldo());
+		
+		c.atualiza(this.selic);
+		
+		System.out.println("Saldo Atualizado R$"+ this.saldoTotal);;
+		
+		this.saldoTotal += c.getSaldo(); 
+		
+		
+	}
+	
+
+	public double getSaldoTotal() {
+		return saldoTotal;
+	}
+
+	public void setSaldoTotal(double saldoTotal) {
+		this.saldoTotal = saldoTotal;
+	}
+
+	public double getSelic() {
+		return selic;
+	}
+
+	public void setSelic(double selic) {
+		this.selic = selic;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
