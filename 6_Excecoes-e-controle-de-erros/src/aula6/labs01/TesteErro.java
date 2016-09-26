fe88bc06e7c8cc5cdec678fc287edf2a2a7d382f
José Malcher Jr <malcher.malch@gmail.com>
2016-09-26 11:41:24 -0300

    Aula 06 Labs 01 - Exercício 03

--- 6_Excecoes-e-controle-de-erros/src/aula6/labs01/TesteErro.java
+++ 6_Excecoes-e-controle-de-erros/src/aula6/labs01/TesteErro.java
@@ -1,29 +1,31 @@
 package aula6.labs01;
 
 public class TesteErro {
-	 public static void main(String[] args) {
-         System.out.println("inicio do main");
-         metodo1();
-         System.out.println("fim do main");
-     }
+	public static void main(String[] args) {
+		System.out.println("inicio do main");
+		metodo1();
+		System.out.println("fim do main");
+	}
 
-     static void metodo1() {
-         System.out.println("inicio do metodo1");
-         metodo2();
-         System.out.println("fim do metodo1");
-     }
+	static void metodo1() {
+		System.out.println("inicio do metodo1");
+		metodo2();
+		System.out.println("fim do metodo1");
+	}
 
-     static void metodo2() {
-         System.out.println("inicio do metodo2");
-         int[] array = new int[10];
-         try{
-         for (int i = 0; i <= 15; i++) {
-             array[i] = i;            
-             System.out.println(i);
-         }
-         }catch(ArrayIndexOutOfBoundsException e){
-        	 System.out.println("ERRO metodo 2 - for" + e);
-         }
-         System.out.println("fim do metodo2");
-     }
+	static void metodo2() {
+		System.out.println("inicio do metodo2");
+		int[] array = new int[10];
+
+		for (int i = 0; i <= 15; i++) {
+			try {
+				array[i] = i;
+				System.out.println(i);
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("ERRO metodo 2 - for" + e);
+			}
+		}
+
+		System.out.println("fim do metodo2");
+	}
 }
