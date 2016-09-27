a53d86210bca4e2971880a9bf7832f20f72fedf8
José Malcher Jr <malcher.malch@gmail.com>
2016-09-27 00:32:16 -0300

    Aula 06 Labs 02 - Exercício 07 e 08

--- 6_Excecoes-e-controle-de-erros/src/aula6/labs02/TestaConta.java
+++ 6_Excecoes-e-controle-de-erros/src/aula6/labs02/TestaConta.java
@@ -3,26 +3,25 @@ package aula6.labs02;
 public class TestaConta {
 
 	public static void main(String[] args) {
-		
+
 		Conta joao = new Conta();
 		joao.deposita(50.0);
-		
-		joao.deposita(-10);
-		
-		try{
-		joao.saca(300.0);
+		try {
+			joao.deposita(-10);
+		} catch (IllegalArgumentException e) {
+			System.out.println("VALOR NEGATIVO - VALOR INVALIDO");
 		}
-		catch(SaldoInsuficienteException e){
-			System.out.println("SALDO INSUFICIENTE "+ e.getSaldoAtual());
+		try {
+			joao.saca(300.0);
+		} catch (SaldoInsuficienteException e) {
+			System.out.println("SALDO INSUFICIENTE " + e.getSaldoAtual());
 		}
-		
-		catch(Exception e){
-			//System.out.println("Não há SALDO!");
+
+		catch (Exception e) {
+			// System.out.println("Não há SALDO!");
 			System.out.println("Um outro ERRO QUALQUER");
 		}
 		System.out.println(joao.getSaldo());
-		
-		
 
 	}
 
